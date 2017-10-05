163838303a57571cb435b10e0870be9751dc1a78
Nina Angelvik <nina.angelvik@gmail.com>
2017-10-05 14:15:59 +0200

    Added area search

--- app/controllers/weather_data_controller.rb
+++ app/controllers/weather_data_controller.rb
@@ -3,7 +3,13 @@ class WeatherDataController < ApplicationController
 	include ActionView::Helpers::TextHelper
 
 	def download
-		filename, records = find_records(params)
+		ret, filename, records = find_records(params)
+
+		unless ret
+			flash[:error] = "Søket er ugyldig."
+			redirect_to root_path and return 
+		end
+		
 		unless records.empty?
 	  	CSV.open(Rails.root.join('tmp', filename), "wb") do |csv|
 		    csv <<  ["Time",
@@ -33,9 +39,13 @@ class WeatherDataController < ApplicationController
 	end
 
 	def get_data
-		filename, records = find_records(params)
+		ret, filename, records = find_records(params)		
 
-		
+		unless ret
+			flash[:error] = "Søket er ugyldig."
+			redirect_to root_path and return 
+		end
+
 		unless records.empty?
 			records_json = []
 			records.each do |r| 
@@ -50,35 +60,58 @@ class WeatherDataController < ApplicationController
 
 	private
 	def find_records(params)
-		if params[:all]
+		case 
+		when params[:all]
 			records = WeatherData.all
-	    filename = "records-all.csv"	
-		elsif params[:fromtime] 
+	    filename = "records-all.csv"
+	  when (params[:fromtime] and params[:totime])
 			from = params[:fromtime].to_time
-			if params[:totime]
-				to = params[:totime].to_time
-			
-				records =	WeatherData.where(:timestamp => from..to)
-				filename = "records-custom-interval.csv"
-			elsif params[:hours]
-				records =	WeatherData.where(:timestamp => from..from + params[:hours].to_i.hour)
-				filename = "records-#{pluralize(params[:hours], 'hour')}.csv"		
-			elsif params[:weeks]
-				records =	WeatherData.where(:timestamp => from..from + params[:weeks].to_i.week)
-				filename = "records-#{pluralize(params[:weeks], 'week')}.csv"
-			elsif params[:months]
-				records =	WeatherData.where(:timestamp => from..from + params[:months].to_i.week)
-				filename = "records-#{pluralize(params[:months], 'month')}.csv"
-			else
-				flash[:error] = "Objektet kan ikke finnes."
-				redirect_to root_path
-			end
-		else
-			flash[:error] = "Vennligst spesifiser hva du vil laste ned."
-			redirect_to root_path
-		end
+			to = params[:totime].to_time
 
-		return filename, records
+			filename = "records-custom.csv"
+		when params[:hour]
+				from = Time.now.beginning_of_hour
+				to = Time.now.end_of_hour
+			
+				filename = "records-hour.csv"	
+		when params[:day]
+				from = Time.now.beginning_of_day
+				to = Time.now.end_of_day
+			
+				filename = "records-24-hours.csv"		
+		when params[:week]
+				from = Time.now.beginning_of_week
+				to = Time.now.end_of_week
+
+				filename = "records-week.csv"
+		when params[:month]
+				from = Time.now.beginning_of_month
+				to = Time.now.end_of_month
+				filename = "records-month.csv"
+		else
+			return false
+		end 		
+		
+		records =	WeatherData.where(:timestamp => from..to)
+
+		if params[:within]
+			within = params[:within][1...-1].split(",")
+			
+			if within.count > 3
+				return false 
+			else  
+				latitude = within[0].to_f
+				longitude = within[1].to_f
+				radius = within[2].to_f
+				area_recs = []
+
+				records.each do |r|
+					area_recs << r if (Geocoder::Calculations.distance_between([latitude, longitude], [r.latitude, r.longitude], :units=>:km) < radius)
+				end
+				records = area_recs
+			end
+		end
+		return true, filename, records
 	end
 
 end
