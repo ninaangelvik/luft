d0aca89f3a67876b8d14d8d38f81ab6bf162839f
Nina Angelvik <nina.angelvik@gmail.com>
2018-04-26 14:04:38 +0200

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,14 @@ Rails.application.routes.draw do
 
 
   # post '/upload', to: 'datafiles#create'
-  get '/download', to: 'home#download'
-  get 'files', to: 'datafiles#index'
-  get 'files/get_id', to: 'datafiles#get_id'
-  get 'files/get_data', to: 'datafiles#get_data'
+  # get '/download', to: 'home#download'
+  # get 'files', to: 'datafiles#index'
+  # get 'files/get_id', to: 'datafiles#get_id'
+  # get 'files/get_data', to: 'datafiles#get_data'
   get '/_ah/health', to: 'home#health'
   get '/liveness_check', to: 'home#alive'
   get '/readiness_check', to: 'home#ready'
-  get '/download', to: 'weather_data#download'
-  get '/get_data', to: 'weather_data#get_data'
-  resource :datafiles, only:[:create]
+  # get '/download', to: 'weather_data#download'
+  # get '/get_data', to: 'weather_data#get_data'
+  # resource :datafiles, only:[:create]
 end
